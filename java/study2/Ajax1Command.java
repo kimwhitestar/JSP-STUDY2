5eb43386ae5de06ef6cefd323d66a0881c0f06de
kimwhitestar <102268288+kimwhitestar@users.noreply.github.com>
2022-05-10 22:05:44 +0900

    Add files via upload

--- java/study2/Ajax1Command.java
+++ java/study2/Ajax1Command.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.Paging;
-import study2.ajax.UserDAO;
-import study2.ajax.UserVO;
+import study2.ajax.database.UserDAO;
+import study2.ajax.database.UserVO;
 
 public class Ajax1Command implements StudyInterface {
 
@@ -18,9 +18,9 @@ public class Ajax1Command implements StudyInterface {
 		UserDAO dao = new UserDAO();
 		
 		Paging paging = new Paging(request, response);
-		int pageNo = request.getParameter("pageNo")==null?0:Integer.parseInt(request.getParameter("pageNo"));
+		int pageNo = request.getParameter("pageNo")==null?1:Integer.parseInt(request.getParameter("pageNo"));
 		paging.setPaging(pageNo, dao.totRecCnt(), 3, 3);
-		
+
 		List<UserVO> vos = dao.searchUserList(paging.getStartIndexNo(), paging.getPageSize());
 		request.setAttribute("vos", vos);
 	}
