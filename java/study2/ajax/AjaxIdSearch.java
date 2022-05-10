4de47c1ebfef16943a5540afa18a07d834bd7be2
kimwhitestar <102268288+kimwhitestar@users.noreply.github.com>
2022-05-10 14:09:15 +0900

    2022.05.10 AJax학원수업

---
+++ java/study2/ajax/AjaxIdSearch.java
@@ -0,0 +1,10 @@
+package study2.ajax;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+//url패턴
+@WebServlet("/asjaxIdSearch")
+public class AjaxIdSearch extends HttpServlet {
+	
+}
